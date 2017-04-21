e321c486d32a89cf628651eef87ce208aa9da68f
alex <jialene@outlook.com>
2017-04-21 17:10:35 +0800

    add some class and interface

--- src/main/java/domain/Config.java
+++ src/main/java/domain/Config.java
@@ -14,4 +14,19 @@ public class Config {
     protected boolean generateHtml = false;
 
 
+    public void setBatchDeletes(boolean batchDeletes) {
+        isBatchDeletes = batchDeletes;
+    }
+
+    public void setBatchInsert(boolean batchInsert) {
+        isBatchInsert = batchInsert;
+    }
+
+    public void setFileUpload(boolean fileUpload) {
+        isFileUpload = fileUpload;
+    }
+
+    public void setGenerateHtml(boolean generateHtml) {
+        this.generateHtml = generateHtml;
+    }
 }
--- src/main/java/generate/Generate.java
+++ src/main/java/generate/Generate.java
@@ -1,12 +1,13 @@
 package generate;
 
+import domain.*;
 import generate.impl.GenerateDataBase;
 
 /**
  * @author alex-jiayu
  * @create 2017-04-14 17:12
  **/
-public class Generate {
+public class Generate{
 
     public static final String AUTHOR = "ALEX/JIAYU";
     public static final String CONTACT = "467146659@qq.com/alexdennis.lam@gmail.com";
@@ -15,7 +16,7 @@ public class Generate {
     //protected String tableName;
 
     ICodeGenerate iCodeGenerate = null;
-    Setting setting = null;
+
 
     public Generate(String classDriver, String url, String userName, String passWord , String tableName){
         this(classDriver , url , userName , passWord , tableName , null);
@@ -28,4 +29,5 @@ public class Generate {
     public boolean generate(String classPackage, String author, String contact, String codePath) {
         return iCodeGenerate.generate(classPackage , author , contact , codePath);
     }
+
 }
